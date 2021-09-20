2dc1207fc5eafb4103ec36ab62b741893985bccc
Rownak <rownak.sust@gmail.com>
2021-09-20 11:30:41 -0600

    Bengio_2003, Negative_Sampling, and sample dynamic negative_sampling

---
+++ Neural_Language_Model_Bengio_2003/basic_lm.py
@@ -0,0 +1,85 @@
+import tensorflow.compat.v1 as tf
+# The version of the tensorflow is v-2.0.
+# But I am using commands/libraries from v-1.0. So disabling the v2 behavior.
+tf.disable_v2_behavior()
+tf.disable_eager_execution()
+
+import numpy as np
+import functools
+
+'''
+Model based off of http://www.jmlr.org/papers/volume3/bengio03a/bengio03a.pdf.
+Works as a simplistic baseline to compare alternative models against.
+'''
+
+class BasicNeuralLM:
+    def __init__(self, conf, data_loader):
+        self.conf = conf
+        self.data = data_loader
+
+        self.global_step = tf.Variable(0, dtype=tf.int32, trainable=False, name='global_step')
+        starter_learning_rate = conf.lr
+        self.learning_rate = tf.train.exponential_decay(starter_learning_rate, self.global_step,
+                                                        100, 0.96, staircase=True)
+
+    def build_placeholders(self):
+        self.train_input = tf.placeholder(tf.int32, shape=[self.conf.batch_size, self.conf.num_steps])
+        self.train_labels = tf.placeholder(tf.int32, shape=[self.conf.batch_size,]) # list of word ids
+
+    def build_embeddings(self):
+        #TODO: Allow for loading of pretrained vectors
+        with tf.device(self.conf.device):
+            with tf.name_scope('embed'):
+                if self.conf.trainable == False:
+                    self.embed_matrix = tf.Variable(tf.random_uniform([self.data.V, self.conf.embed_dim], -1.0, 1.0),
+                                                    trainable=False,
+                                                    name='embed_matrix')
+                else:
+                    self.embed_matrix = tf.Variable(tf.constant(0.0, shape=[self.data.V, self.conf.embed_dim]),
+                                                    name='embed_matrix')
+
+    def build_loss(self):
+        with tf.device(self.conf.device):
+            with tf.name_scope('loss'):
+                # Output bias
+                b = tf.Variable(tf.constant([self.data.V], 1.0), name='bias1')
+                # Allow for direct connections to softmax layer (input to output)
+                if self.conf.direct_connections == True:
+                    W = tf.Variable(tf.random_normal([self.conf.embed_dim * self.conf.num_steps, self.data.V], -1.0, 1.0))
+                else:
+                    W = tf.Variable(np.zeros([self.conf.embed_dim * self.conf.num_steps, self.data.V]), trainable=False)
+
+                # Input to hidden
+                H = tf.Variable(tf.random_uniform([self.conf.num_steps * self.conf.embed_dim, self.conf.hidden_dim], -1.0, 1.0))
+                # Hidden bias
+                d = tf.Variable(tf.random_uniform([self.conf.hidden_dim]))
+                # Hidden to output
+                U = tf.Variable(tf.random_uniform([self.conf.hidden_dim, self.data.V]))
+
+                input = tf.nn.embedding_lookup(self.embed_matrix, self.train_input) # batch_size x num_steps x embed_dim
+                input_mat = tf.reshape(input, (self.conf.batch_size, self.conf.num_steps * self.conf.embed_dim)) # reshape to be batch_size x concatenated word vectors
+
+                hidden = tf.tanh(tf.matmul(input_mat, H) + d)
+                hidden2out = tf.matmul(hidden, U) + b
+                self.logits = tf.matmul(input_mat, W) + hidden2out
+
+                self.loss = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=tf.one_hot(self.train_labels, self.data.V))
+
+    def build_optimizer(self):
+        with tf.device(self.conf.device):
+            self.optimizer = tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.loss, global_step=self.global_step)
+
+    def build_summaries(self):
+        # For tensorboard visualization, run main.py then in command line prompt type:
+        # tensorboard --logdir="./graphs" --port 6006
+        # then open browser to http://localhost:6006/
+        with tf.name_scope("summaries"):
+            tf.summary.scalar("loss", self.loss)
+            self.summary_op = tf.summary.merge_all()
+
+    def build_graph(self):
+        self.build_placeholders()
+        self.build_embeddings()
+        self.build_loss()
+        self.build_optimizer()
+        #self.build_summaries()
---
+++ Neural_Language_Model_Bengio_2003/utils.py
@@ -0,0 +1,129 @@
+import numpy as np
+from nltk import word_tokenize
+from collections import Counter
+import random
+
+#TODO: Handle train/test/validation data
+    #TODO: Datashuffling 2/3, 1/6, 1/6
+    #TODO: Store them separately
+#TODO: Write _make_batches
+#TODO: Define method to input <EOS> tokens
+#TODO: Global variable called numval batches, and num_testbatches
+
+class DataLoader(object):
+    def __init__(self, filename, batch_size, num_steps):
+        '''
+        :param filename: path to corpus
+        :param batch_size: size of minibatches
+        :return: object with useful tools for handling corpus
+        '''
+        self.window = num_steps
+        filereader = open(filename, 'r')
+        lines = []
+        counts = Counter()
+        #extract sentences
+        for line in filereader.readlines():
+            tokenized = word_tokenize(line.lower())
+            counts.update(tokenized)
+            lines.append(tokenized)
+        #consolidate vocabulary
+        self.vocab = []
+        for w in counts:
+            if counts[w] >= 2 :
+                self.vocab.append(w)
+        self.vocab.append("<UNK>")
+        self.vocab.append("#")
+        self.V = len(self.vocab)
+
+        #filter low frequqency words
+        for line in lines:
+            for i in range(len(line)):
+                if line[i] not in self.vocab:
+                    line[i] = "<UNK>"
+
+        np.random.seed(31415)
+        #shuffle the lines and split them in train test and validation
+        random.shuffle(lines)
+        num_sents= len(lines)
+        train_sents, test_sents, val_sents = lines[:int(num_sents*0.7)] ,lines[int(num_sents*0.7):int(num_sents*0.85)],lines[int(num_sents*0.85):]
+
+        self.idx2word = dict(enumerate(self.vocab))
+        self.word2idx = {value:key for (key, value) in self.idx2word.items()}
+
+        # A dictionary that contains batch information as a triple consisting of:
+        # The list of batch matrices
+        # The list of label vectors
+        # The index where the next batchshould be extracted
+        self.all_batch_triples={}
+
+        #populate the triplets via the _make_batches method
+        self.all_batch_triples["train"]=  self._make_batches(batch_size, train_sents)
+        self.all_batch_triples["test"] = self._make_batches(batch_size, test_sents)
+        self.all_batch_triples["validation"] = self._make_batches(batch_size, val_sents)
+
+
+        self.num_batches = {}
+        self.num_batches["train"] = len(self.all_batch_triples["train"][0])
+        self.num_batches["test"] = len(self.all_batch_triples["test"][0])
+        self.num_batches["validation"] = len(self.all_batch_triples["validation"][0])
+        assert self.num_batches['validation'] > 0 and self.num_batches['train'] > 0, "Error: batch size too big, try lowering."
+
+    def _cast_index(self):
+        '''
+        :param sequence: input list of tokens to be cast as their indexes
+        :return: list of same length of sequence of indices corresponding to tokens
+        '''
+        return [self.word2idx[w] for w in self.data if w in self.word2idx]
+
+    def _make_batches(self, batch_size, sentences, shuffle=False):
+        '''
+        :param batch_size: size of minibatches
+        :return: object consisting of all batches in corpus to be loaded with _load_batches
+        '''
+        n = self.window
+        batches = []
+        labels = []
+        for sentence in sentences:
+            current_batch = []
+            current_labels = []
+            #Pad sentence with out of sentence tokens
+            sentence = ["#"] * (n-1) + sentence + ["#"] * (n-1)
+            # compile the matrix
+            for begin_ind in range(len(sentence)-n-1):
+                chunk= sentence[begin_ind:begin_ind+n]
+                chunk_ids=[self.word2idx[word] for word in chunk]
+                current_batch.append(chunk_ids)
+                current_labels.append(self.word2idx[sentence[begin_ind+n+1]])
+                if len(current_batch) == batch_size:
+                    batches.append(current_batch)
+                    labels.append(current_labels)
+                    current_labels=[]
+                    current_batch=[]
+        return [batches, labels, 0]
+
+    def load_batches(self, kind="train"):
+        random.seed(random.randint)
+        batch_pairs=list(zip(self.all_batch_triples[kind][0],self.all_batch_triples[kind][1]))
+        random.shuffle(batch_pairs)
+        random.seed(31415)
+        return batch_pairs
+
+class SmallConfig:
+    def __init__(self):
+        self.batch_size = 100
+        self.num_steps = 6
+        self.num_epochs = 50
+        self.skip_step = 2
+        self.embed_dim = 100
+        self.hidden_dim = 60
+        self.lr = 0.01
+        self.direct_connections = True
+        self.device = "/cpu:0"
+        self.trainable = True
+
+
+class MediumConfig:
+    pass
+
+class LargeConfig:
+    pass
